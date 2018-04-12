b831b3819e157f27ea2fad8b00b34f47eed84c0a
Planters <Planters@a.a>
2018-04-12 15:17:01 +0200

    Transfered Testing To A Different Module
    Testing has been moved into a separate module to allow the API to be
    used without the requirement of a testing framework.

---
+++ src/hockey.mask.json/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Defines a JSON parsing API.
+ * 
+ * @author Planters
+ */
+module hockey.mask.json {
+	
+	exports hockey.mask.json;
+	exports hockey.mask.json.io;
+	
+	requires transitive java.base;
+	
+}
---
+++ src/hockey.mask.test/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Defines a testing module for the module hockey.mask.json.
+ * 
+ * @author Planters
+ */
+module hockey.mask.test {
+
+	exports hockey.mask.test;
+	
+	requires hockey.mask.json;
+	requires transitive java.base;
+	requires koro.sensei.tester;
+	
+}
