339a8c94b2b39049f05e8eabfdfe3ff4d551fb3b
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-06-14 00:02:50 +0200

    Implemented JSON Null
    The internal representation of JSON nulls has been changed to a
    standalone class.

---
+++ src/hockey.mask.json/hockey/mask/json/JsonNull.java
@@ -0,0 +1,92 @@
+package hockey.mask.json;
+
+import hockey.mask.json.parser.JsonParser;
+
+/**
+ * The JsonNull class represents a JSON formatted null value.
+ * 
+ * @author Planters
+ *
+ */
+public class JsonNull {
+	
+	/**
+	 * The JSON representation of a null value.
+	 */
+	public static final String JSON_NULL_VALUE = "null";
+
+	public JsonNull() {
+		// empty constructor
+	}
+	
+	/**
+	 * Transforms the specified string to a JSON formatted string.
+	 * If the string is null, the JSON representation of null is returned.
+	 * 
+	 * @return the JSON string representation of null
+	 */
+	public String toJson() {
+		return JsonNull.JSON_NULL_VALUE;
+	}
+
+	/**
+	 * Parse the specified JSON formatted string and return its internal representation.
+	 * This basically strips the string from the JSON string identifier characters.
+	 * 
+	 * @param jsonString - the JSON formatted string
+	 * @return the internal representation of the JSON formatted string
+	 * @throws JsonStandardException if the string was not JSON formatted
+	 * @throws NullPointerException - if null is passed as JSON input string
+	 */
+	public static JsonNull parse(String jsonString) throws JsonStandardException {
+		if (jsonString != null) {
+			JsonParser jp = new JsonParser(jsonString);
+			JsonNull parsedNull = JsonNull.parseNext(jp);
+			jp.skipWhitespace(); // needed for checking against garbage data#
+			if (!jp.hasNext()) {
+				return parsedNull;
+			} else { // the string should not contain any more garbage data
+				throw new JsonStandardException(String.format("The string \"%s\" is not a pure JSON null.", 
+						jsonString)); 
+			}
+		} else {
+			throw new NullPointerException("A JSON formatted string may not be null.");
+		}
+	}
+	
+	/**
+	 * Parse the next JSON formatted null from the specified JSON parser and return its 
+	 * internal representation.
+	 * 
+	 * @param parser - the parser to retrieve the JSON formatted string from
+	 * @return the internal representation of the JSON formatted string
+	 * @throws JsonStandardException if the next element in the parser is not a JSON formatted null
+	 * @throws NullPointerException - if null is passed as JSON parser
+	 */
+	public static JsonNull parseNext(JsonParser parser) throws JsonStandardException {
+		if (parser != null) {
+			int startingPosition = parser.getPosition();
+			parser.skipWhitespace();
+			if (parser.isNext(JsonNull.JSON_NULL_VALUE, true)) {
+				return new JsonNull();
+			} else {
+				parser.setPosition(startingPosition); // the parser should not be modified
+				throw new JsonStandardException(String.format("The next element in the JSON parser "
+						+ "%s is not a JSON null.", parser));
+			}
+		} else {
+			throw new NullPointerException("The JSON parser may not be null.");
+		}
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return obj != null && obj instanceof JsonNull;
+	}
+	
+	@Override
+	public String toString() {
+		return "null";
+	}
+	
+}
---
+++ src/hockey.mask.test/hockey/mask/test/JsonNullTesting.java
@@ -0,0 +1,178 @@
+package hockey.mask.test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import hockey.mask.json.JsonNull;
+import hockey.mask.json.JsonStandardException;
+import hockey.mask.json.parser.JsonParser;
+import koro.sensei.tester.TestFailureException;
+import koro.sensei.tester.TestSubject;
+
+/**
+ * The JsonNullTesting class test the JsonNull class for correct functionality.
+ * 
+ * @author Planters
+ *
+ */
+public class JsonNullTesting implements TestSubject {
+
+	private static final Random RANDOM = new Random();
+	
+	@Override
+	public void runAllTests() throws TestFailureException {
+		JsonNullTesting.testConstructors();
+		JsonNullTesting.testToJson();
+		JsonNullTesting.testParsing();
+		JsonNullTesting.testParsingNext();
+	}
+	
+	/**
+	 * Test the constructors and some basic equality.
+	 * 
+	 * @throws TestFailureException the test did fail
+	 */
+	private static void testConstructors() throws TestFailureException {
+		JsonNull firstNull = new JsonNull();
+		JsonNull secondNull = new JsonNull();
+		TestSubject.assertTestCondition(firstNull.equals(secondNull), 
+				String.format("The JSON null %s should equal %s", 
+						firstNull, secondNull));
+		TestSubject.assertTestCondition(!firstNull.equals(null), 
+				String.format("The JSON null %s should not equal a Java null.", firstNull));
+	}
+	
+	/**
+	 * Test the conversion of null to a JSON formatted string.
+	 * 
+	 * @throws TestFailureException the test did fail
+	 */
+	private static void testToJson() throws TestFailureException {
+		JsonNull testNull = new JsonNull();
+		TestSubject.assertTestCondition(testNull.toJson().equals(JsonNull.JSON_NULL_VALUE), 
+				String.format("The JSON representation of null "
+						+ "should equal the value \"%s\", but is \"%s\".", 
+						JsonNull.JSON_NULL_VALUE, testNull.toJson()));
+	}
+	
+	/**
+	 * Test parsing JSON formatted strings.
+	 * 
+	 * @throws TestFailureException the test did fail
+	 */
+	private static void testParsing() throws TestFailureException {
+		// test JSON null strings
+		try {
+			JsonNull.parse(null);
+			throw new TestFailureException("Parsing of null for a JSON string should fail.");
+		} catch (JsonStandardException e) {
+			throw new TestFailureException("Parsing of null for a JSON string should throw a "
+					+ "NullPointerException.", e);
+		} catch (NullPointerException e) {
+			/*
+			 * Do nothing as this is expected behaviour.
+			 */
+		}
+		// test empty strings
+		try {
+			JsonNull.parse("");
+			throw new TestFailureException("Parsing of empty for a JSON null should fail.");
+		} catch (JsonStandardException e) {
+			/*
+			 * Do nothing as this is expected behaviour.
+			 */
+		}
+		// test parsing the correct string
+		try {
+			JsonNull.parse(JsonNull.JSON_NULL_VALUE);
+		} catch (JsonStandardException e) {
+			throw new TestFailureException(String.format("Parsing \"%s\" should result in a "
+					+ " Json null", JsonNull.JSON_NULL_VALUE), e);
+		}
+		// test random strings
+		for (int i = 0; i < 10000; i++) {
+			// create random strings
+			byte[] randomString = new byte[JsonNullTesting.RANDOM.nextInt(200)];
+			JsonNullTesting.RANDOM.nextBytes(randomString);
+			String testString = (new String(randomString)).replace(JsonNull.JSON_NULL_VALUE, "");
+			if (testString.length() > 0) {
+				try {
+					JsonNull parsedJsonNull = JsonNull.parse(testString);
+					throw new TestFailureException(String.format("Parsing the string \"%s\" "
+							+ "as JSON null should fail, but resulted into %s.", testString, 
+							parsedJsonNull));
+				} catch (JsonStandardException e) {
+					/*
+					 * Do nothing as this is expected behaviour.
+					 */
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Test sequentially parsing JSON formatted strings.
+	 * 
+	 * @throws TestFailureException the test did fail
+	 */
+	private static void testParsingNext() throws TestFailureException {
+		// test null
+		try {
+			JsonNull.parseNext(null);
+			throw new TestFailureException("Parsing of null for a JSON parser should fail.");
+		} catch (NullPointerException e) {
+			/*
+			 * Do nothing as this is expected behaviour.
+			 */
+		} catch (JsonStandardException e) {
+			throw new TestFailureException("Parsing of null for a JSON string should throw a "
+					+ "NullPointerException.", e);
+		}
+		// test JSON string
+		for (int i = 0; i < 10000; i++) {
+			JsonNull[] jsonTestNulls = new JsonNull[JsonNullTesting.RANDOM.nextInt(20) + 1];
+			String testString = "  "; // Some whitespace
+			for (int j = 0; j < jsonTestNulls.length; j++) {
+				jsonTestNulls[j] = new JsonNull();
+				testString += jsonTestNulls[j].toJson() + "   ";
+			}
+			try {
+				JsonParser jp = new JsonParser(testString);
+				JsonNull[] parsedJsonNulls = new JsonNull[jsonTestNulls.length];
+				for (int j = 0; j < jsonTestNulls.length; j++) {
+					parsedJsonNulls[j] = JsonNull.parseNext(jp);
+				}
+				TestSubject.assertTestCondition(Arrays.equals(jsonTestNulls, parsedJsonNulls),
+					String.format("The parsed JSON nulls %s should equal %s.", 
+							Arrays.toString(parsedJsonNulls), 
+							Arrays.toString(jsonTestNulls)));
+			} catch (JsonStandardException e) {
+				throw new TestFailureException(String.format("Parsing of the string \"%s\" as JSON "
+						+ "nulls failed.", testString
+						), e);
+			}
+		}
+		// test reseting position mark after exception
+		String testString = JsonNull.JSON_NULL_VALUE;
+		// remove the last "l" so an exception will be raised
+		testString = testString.substring(0, testString.length() - 1);
+		try {
+			JsonParser jp = new JsonParser(testString);
+			int initialPosition = jp.getPosition();
+			try {
+				JsonNull.parseNext(jp);
+				throw new TestFailureException(String.format("Parsing of the string \"%s\" as JSON "
+						+ "null should fail, but resulted in the parser %s.", 
+						testString, jp));
+			} catch (JsonStandardException e) {
+				TestSubject.assertTestCondition(jp.getPosition() == initialPosition,
+						String.format("The JSON parser %s should be reset to position %s after "
+								+ "failing to parse, but is at %s.", 
+								jp, initialPosition, jp.getPosition()));
+			}
+		} catch (JsonStandardException e) {
+			throw new TestFailureException("Creating the JSON parser failed.", e);
+		}
+	}
+
+}
